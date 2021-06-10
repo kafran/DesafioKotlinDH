aafb30f8ac559c3e323c18d85607f390108060fe
Kolmar Kafran <kolmar@kafran.net>
2021-06-10 20:26:53 -0300

    Removes professorTitular and professorAdjunto from constructor.

--- src/Curso.kt
+++ src/Curso.kt
@@ -1,11 +1,12 @@
 class Curso(
     val nome: String,
     val codigo: Int,
-    val professorTitular: ProfessorTitular,
-    val professorAdjunto: ProfessorAdjunto,
     val capacidadeMax: Int,
     private var alunosMatriculados: MutableList<Aluno> = mutableListOf()
 ) {
+    var professorTitular: ProfessorTitular? = null
+    var professorAdjunto: ProfessorAdjunto? = null
+
     override fun equals(other: Any?): Boolean {
         return (other is Curso) && this.codigo == other.codigo
     }
