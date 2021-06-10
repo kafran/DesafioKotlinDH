cb03ea6ce1b72716e7f17de189a6177ca9d9ded5
Kolmar Kafran <kolmar@kafran.net>
2021-06-10 15:21:49 -0300

    Modifies Professor class. Creates the classes ProfessorTitular and ProfessorAdjunto.
    Parte D: The Professor class was modified to abstract class. The choice for abstract class instead of interface was due to the implementation of equals in the abstract class itself.

--- src/Professor.kt
+++ src/Professor.kt
@@ -1,10 +1,30 @@
-class Professor(
-    val nome: String,
-    val sobrenome: String,
-    val tempoDeCasa: Int,
-    val codigo: Int
-) {
+abstract class Professor {
+    abstract val nome: String
+    abstract val sobrenome: String
+    abstract val tempoDeCasa: Int
+    abstract val codigo: Int
+
     override fun equals(other: Any?): Boolean {
         return (other is Professor) && this.codigo == other.codigo
     }
+}
+
+class ProfessorTitular(
+    override val nome: String,
+    override val sobrenome: String,
+    override val tempoDeCasa: Int,
+    override val codigo: Int,
+    val especialidade: String
+) : Professor() {
+    //TODO
+}
+
+class ProfessorAdjunto(
+    override val nome: String,
+    override val sobrenome: String,
+    override val tempoDeCasa: Int,
+    override val codigo: Int,
+    val horasMonitoria: Int
+) : Professor() {
+    //TODO
 }
