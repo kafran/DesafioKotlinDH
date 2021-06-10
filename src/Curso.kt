bdb292ba0e1fd9eb0e7f52db0dfdf35bcf22e76c
Kolmar Kafran <kolmar@kafran.net>
2021-06-10 16:04:26 -0300

    Modifica a classe Curso.
    Adiciona professores adjunto e titular, capacidade máxima e lista de alunos matriculados.

--- src/Curso.kt
+++ src/Curso.kt
@@ -1,6 +1,10 @@
 class Curso(
     val nome: String,
-    val codigo: Int
+    val codigo: Int,
+    val professorTitular: ProfessorTitular,
+    val professorAdjunto: ProfessorAdjunto,
+    val capacidadeMax: Int,
+    var alunosMatriculados: MutableList<Aluno> = mutableListOf()
 ) {
     override fun equals(other: Any?): Boolean {
         return (other is Curso) && this.codigo == other.codigo
