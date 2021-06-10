9c3fb8da130d3a85ae082baf5befa93de7895925
Kolmar Kafran <kolmar@kafran.net>
2021-06-10 16:25:13 -0300

    Parte G: Created class DigitalHouseManager.

---
+++ src/DigitalHouseManager.kt
@@ -0,0 +1,6 @@
+class DigitalHouseManager {
+    var alunos: MutableList<Aluno> = mutableListOf()
+    var professores: MutableList<Professor> = mutableListOf()
+    var cursos: MutableList<Curso> = mutableListOf()
+    var matriculas: MutableList<Matricula> = mutableListOf()
+}
